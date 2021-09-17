25c2a14c1c847652fe0cacb93cc53a5fbc878a68
Jitender <jkyadav1006@gmail.com>
2021-09-17 17:43:56 +0530

    Modified user and server interface

--- ClientApp.py
+++ ClientApp.py
@@ -17,16 +17,16 @@ def sendMessage():
         if read[0] == "CLOSE":
             active = False
         elif read[0][0] != "@":
-            print("Please tag a user using @.")
+            print("Please tag a user using @.\n")
         elif len(read) < 2:
-            print("Please do not send empty messages.")
+            print("Please do not send empty messages.\n")
         else:
             send_user = read[0][1:]
             content = " ".join(read[1:])
             send_msg = "SEND " + send_user + " \nContent-length: " + str(len(content)) + " \n\n" + content
             send_sock.send(bytes(send_msg, 'utf-8'))
             reply = str(send_sock.recv(1024), 'utf-8')
-            print(reply)
+            print(reply.split("\n")[0], "\n")
     send_sock.close()
 
 def recvMessage():
@@ -44,17 +44,17 @@ def recvMessage():
             sender = get[0].split(" ")[1]
             length = int(get[1].split(" ")[1])
             msg = get[3][0:length]
-            print("\nMessage From " + sender + ":\n" + msg)
+            print("New Message From " + sender + ":\n" + msg, "\n")
     recv_sock.close()
 
 
 
-print("Welcome to Jitender's Chat Application. Thank you for choosing us. <3")
+print("Welcome to Jitender's Chat Application. Thank you for choosing us. <3\n")
 
 # get username and hostname from user
-username = input("\nEnter a valid username.\n")
+username = input("Enter a valid username.\n")
 user_check = False
-host = input("\nEnter the server's IPv4 address. For local server, use 'local'.\n")
+host = input("Enter the server's IPv4 address. For local server, use 'local'.\n")
 if host == "local":
     host = socket.gethostname()
 host_check = False
@@ -66,14 +66,12 @@ while not (host_check and user_check):
         send_sock.connect((host, 10000))
         send_sock.send(bytes("REGISTER TOSEND " + username + "\n\n", 'utf-8'))
         send_response = str(send_sock.recv(1024), 'utf-8')
-        print("\n" + send_response.split("\n")[0])
         send_response = (send_response.strip().split("\n")[0]).split(" ")
         
         # make connection for recieving
         recv_sock.connect((host, 10000))
         recv_sock.send(bytes("REGISTER TORECV " + username + "\n\n", 'utf-8'))
         recv_response = str(recv_sock.recv(1024), 'utf-8')
-        print(recv_response.split("\n")[0])
         recv_response = (recv_response.strip().split("\n")[0]).split(" ")
         
         if recv_response[0] == "ERROR" and recv_response[1] == "100":
@@ -89,11 +87,11 @@ while not (host_check and user_check):
         
         # validation message
         if send_response[0] == "REGISTERED" and recv_response[0] == "REGISTERED":
-            print("\nNew User " + send_response[2] + " registered at server.\n")
-            print("To start a conversation, tag a user. To end application, type CLOSE.\n")
+            print("\n" + send_response[2] + ", you have been registered at the server.")
+            print("To start a conversation, tag a user. Tag ALL to broadcast. To end application, type CLOSE.\n")
         
-    except ConnectionRefusedError:
-        host = input("\nServer could not be connected.\nEnter a valid server's IPv4 address. For local server, use 'local'.\n")
+    except:
+        host = input("\nServer could not be connected.\n\nEnter a valid server's IPv4 address. For local server, use 'local'.\n")
         if host == "local":
             host = socket.gethostname()
         send_sock.close()
--- ServerApp.py
+++ ServerApp.py
@@ -11,7 +11,7 @@ host = socket.gethostname()
 server.bind((host, 10000))
 server.listen()
 
-def sendReqAccept(reg_req, conn_socket):
+def sendReqAccept(reg_req, conn_socket, addr):
     # analyse registration request for TOSEND and send response
     global send_users
     global recv_users
@@ -20,32 +20,41 @@ def sendReqAccept(reg_req, conn_socket):
     if len(reg_req) == 3 and user[0].isalpha() and user.isalnum() and user not in send_users.keys() and not user == "ALL":
         conn_socket.send(bytes("REGISTERED TOSEND " + user + "\n\n", 'utf-8'))
         send_users[user] = conn_socket
+        print("New user '" + user + "' connected to send messages at", addr)
     else:
         conn_socket.send(bytes("ERROR 100 Malformed username\n\n", 'utf-8'))
 
     # send message to requested clients
     while True:
-        request = str(conn_socket.recv(4096), 'utf-8').split("\n")
-        if len(request) < 4 or len(request[0].split(" ")) < 2 or len(request[1].split(" ")) < 2:
-            conn_socket.send(bytes("ERROR 103 Header incomplete\n\n", 'utf-8'))
-        else:
-            send_to = request[0].split(" ")[1]
-            msg = request[3][0:int(request[1].split(" ")[1])]
-            if not send_to == "ALL" and send_to not in recv_users.keys():
-                conn_socket.send(bytes("ERROR 102 Unable to send\n\n", 'utf-8'))
+        try:
+            request = str(conn_socket.recv(4096), 'utf-8').split("\n")
+            if len(request) < 4 or len(request[0].split(" ")) < 2 or len(request[1].split(" ")) < 2:
+                conn_socket.send(bytes("ERROR 103 Header incomplete\n\n", 'utf-8'))
             else:
-                try:
-                    if send_to == "ALL":
-                        for reciever in recv_users.keys():
-                            if not reciever == user:
-                                recv_users[reciever].send(bytes("FORWARD " + user + "\nContent-length: " + str(len(msg)) + "\n\n" + msg, 'utf-8'))
-                    else:
-                        recv_users[send_to].send(bytes("FORWARD " + user + "\nContent-length: " + str(len(msg)) + "\n\n" + msg, 'utf-8'))
-                    conn_socket.send(bytes("SENT " + send_to + "\n\n", 'utf-8'))
-                except:
+                send_to = request[0].split(" ")[1]
+                msg = request[3][0:int(request[1].split(" ")[1])]
+                if not send_to == "ALL" and send_to not in recv_users.keys():
                     conn_socket.send(bytes("ERROR 102 Unable to send\n\n", 'utf-8'))
+                else:
+                    try:
+                        if send_to == "ALL":
+                            for reciever in recv_users.keys():
+                                if not reciever == user:
+                                    recv_users[reciever].send(bytes("FORWARD " + user + "\nContent-length: " + str(len(msg)) + "\n\n" + msg, 'utf-8'))
+                        else:
+                            recv_users[send_to].send(bytes("FORWARD " + user + "\nContent-length: " + str(len(msg)) + "\n\n" + msg, 'utf-8'))
+                        conn_socket.send(bytes("SENT " + send_to + "\n\n", 'utf-8'))
+                    except:
+                        conn_socket.send(bytes("ERROR 102 Unable to send\n\n", 'utf-8'))
+        except BrokenPipeError:
+            send_users[user].close()
+            recv_users[user].close()
+            print("User '" + user + "' at", addr, "has left the server.\n")
+            del send_users[user]
+            del recv_users[user]
+            break
 
-def recvReqAccept(reg_req, conn_socket):
+def recvReqAccept(reg_req, conn_socket, addr):
     # analyse registration request for TORECV and send response
     global send_users
     global recv_users
@@ -54,6 +63,7 @@ def recvReqAccept(reg_req, conn_socket):
     if len(reg_req) == 3 and user[0].isalpha() and user.isalnum() and user not in recv_users.keys() and not user == "ALL":
         conn_socket.send(bytes("REGISTERED TORECV " + user + "\n\n", 'utf-8'))
         recv_users[user] = conn_socket
+        print("New user '" + user + "' connected to receive messages at", addr, "\n")
     else:
         conn_socket.send(bytes("ERROR 100 Malformed username\n\n", 'utf-8'))
 
@@ -65,11 +75,11 @@ while True:
     if reg_req[0] != "REGISTER":
         req.send(byte("ERROR 101 No User Registered\n\n", 'utf-8'))
     elif reg_req[1] == 'TOSEND':
-        thr = threading.Thread(target = sendReqAccept, args = (reg_req, req))
+        thr = threading.Thread(target = sendReqAccept, args = (reg_req, req, addr))
         threads.append(thr)
         thr.start()
     else:
-        thr = threading.Thread(target = recvReqAccept, args = (reg_req, req))
+        thr = threading.Thread(target = recvReqAccept, args = (reg_req, req, addr))
         threads.append(thr)
         thr.start()
 
